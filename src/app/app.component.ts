9ebefc325ac8caef3e47e677cf5492ce0d16bf74
= <zainr7989@gmail.com>
2021-06-07 17:01:40 -0400

    Add CSS to our component (also forgot to add the HTML template before).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,24 @@ import { Component } from '@angular/core';
     `   // example of an "inline-template"
     */
     templateUrl: './app.component.html',
+    // CSS goes below
+    /*
+    styles: [`
+        h1 { color: #ffffff; }
+        `, `
+        .description {
+            color: green;
+            font-style: italic;
+        }
+    `],
+    */
+    // Note: you can use more than 1 CSS file for one component
+    // also note: Angular has additional logic that
+        // will scope these styles to JUST the components you apply them to
+    styleUrls: [
+       './app.component.css',
+    ]
+
 })
 export class AppComponent {
 
